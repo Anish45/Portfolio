7a3474d60f9c61701219a529068c477bc3f35083
Anish45 <56783105+Anish45@users.noreply.github.com>
2020-11-26 20:09:26 +0545

    Add files via upload

--- javascript/script.js
+++ javascript/script.js
@@ -1,1 +1,35 @@
-
+function validateForm() {       //This is the function
+  //storing the values in the variables
+  var name = document.forms["myForm"]["name"].value;
+  var address = document.forms["myForm"]["address"].value;
+  var phone = document.forms["myForm"]["phone"].value;
+  var email = document.forms["myForm"]["email"].value;
+  var feedback = document.forms["myForm"]["feedback"].value;
+  
+  // if(name =="" ||address =="" ||phone =="" ||email =="" ||feedback ==""){
+  // 	alert("please fill the field");
+  // }
+  // else{
+  // 	alert("Thank you " + name);
+  // }
+  //checking the condition for the empty values
+if(name ==""){  
+	alert("please fill the field name")
+}
+else if(address ==""){
+	alert("please fill the field address")
+}
+else if(phone ==""){
+	alert("please fill the field phone")
+}
+else if(email ==""){
+	alert("please fill the field email")
+}
+else if(feedback ==""){
+	alert("please fill the field feedback")
+}
+//if all the above conditions are false then else is executed
+else{
+	alert("Thank you " + name + " for contacting me and giving feedback")
+}
+}
